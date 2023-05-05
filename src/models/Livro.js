dac570229dbbe5136a4f9d9aa9692b166f527acd
Mykon <helvis89@gmail.com>
2023-05-05 17:59:19 -0300

    linkando livros e autores

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,8 +3,12 @@ import mongoose from 'mongoose';
 const livroSchema = new mongoose.Schema({
   id: { type: String },
   titulo: { type: String, required: true },
-  autor: { type: String, require: true },
-  editora: { type: String, require: true },
+  autor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'autores',
+    required: true,
+  },
+  editora: { type: String, required: true },
   numeroPaginas: { type: Number },
 });
 
